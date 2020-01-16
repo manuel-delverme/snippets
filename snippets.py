15f208df04460f9dd01de10c1d3a986e4f38d590
manuel <manuel.delverme@gmail.com>
2020-01-16 10:48:55 +0100

    imports and tabs

--- snippets.py
+++ snippets.py
@@ -1,50 +1,57 @@
+import os
+import pickle
+import sys
+from contextlib import contextmanager
+from functools import lru_cache, _lru_cache_wrapper
+from typing import Callable, Union, Tuple
+
+
 def to_tuples(list_of_lists):
-  tuple_of_tuples = []
-  for item in list_of_lists:
-    if isinstance(item, list):
-      item = to_tuples(item)
-    tuple_of_tuples.append(item)
-  return tuple(tuple_of_tuples)
+    tuple_of_tuples = []
+    for item in list_of_lists:
+        if isinstance(item, list):
+            item = to_tuples(item)
+        tuple_of_tuples.append(item)
+    return tuple(tuple_of_tuples)
 
 
 def disk_cache(f: Callable) -> _lru_cache_wrapper:
-  @lru_cache(maxsize=1024)
-  def wrapper(*args, **kwargs):
-    fid = f.__name__
-    cache_file = "cache/{}".format(fid)
-    if args:
-      if not os.path.exists(cache_file):
-        os.makedirs(cache_file)
-      fid = fid + "/" + "::".join(str(arg) for arg in args).replace("/", "_")
-      cache_file = "cache/{}".format(fid)
-    cache_file += ".pkl"
-    try:
-      with open(cache_file, "rb") as fin:
-        retr = pickle.load(fin)
-    except FileNotFoundError:
-      retr = f(*args, **kwargs)
-      with open(cache_file, "wb") as fout:
-        pickle.dump(retr, fout)
-    return retr
+    @lru_cache(maxsize=1024)
+    def wrapper(*args, **kwargs):
+        fid = f.__name__
+        cache_file = "cache/{}".format(fid)
+        if args:
+            if not os.path.exists(cache_file):
+                os.makedirs(cache_file)
+            fid = fid + "/" + "::".join(str(arg) for arg in args).replace("/", "_")
+            cache_file = "cache/{}".format(fid)
+        cache_file += ".pkl"
+        try:
+            with open(cache_file, "rb") as fin:
+                retr = pickle.load(fin)
+        except FileNotFoundError:
+            retr = f(*args, **kwargs)
+            with open(cache_file, "wb") as fout:
+                pickle.dump(retr, fout)
+        return retr
 
-  return wrapper
+    return wrapper
 
 
 @contextmanager
 def suppress_stdout():
-  with open(os.devnull, "w") as devnull:
-    old_stdout = sys.stdout
-    sys.stdout = devnull
-    try:
-      yield
-    finally:
-      sys.stdout = old_stdout
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
 
 
 def one_hot_actions(actions: Union[Tuple[Tuple[int, int]], Tuple[Tuple[int, int, int]]], num_actions: int) -> ndarray:
-  possible_actions = np.zeros((len(actions), num_actions), np.float32)
-  for row, pas in enumerate(actions):
-    for pa in pas:
-      possible_actions[row, pa] = 1
-  return possible_actions
-
+    possible_actions = np.zeros((len(actions), num_actions), np.float32)
+    for row, pas in enumerate(actions):
+        for pa in pas:
+            possible_actions[row, pa] = 1
+    return possible_actions
